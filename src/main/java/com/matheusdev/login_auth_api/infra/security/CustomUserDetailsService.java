e2253095688ad7cd86d903d005a2288b7744c839
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-06-27 20:50:31 -0300

    fix: notation @component

--- src/main/java/com/matheusdev/login_auth_api/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/matheusdev/login_auth_api/infra/security/CustomUserDetailsService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import com.matheusdev.login_auth_api.domain.user.User;
 import com.matheusdev.login_auth_api.repositories.UserRepository;
 
+@Component
 public class CustomUserDetailsService implements UserDetailsService{
     @Autowired
     private UserRepository repository;
