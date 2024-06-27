447024e512b0458ef36509293a6145741107f0c4
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-06-27 19:07:17 -0300

    feat: create user repository

---
+++ src/main/java/com/matheusdev/login_auth_api/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.matheusdev.login_auth_api.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.matheusdev.login_auth_api.domain.user.User;
+
+public interface UserRepository extends JpaRepository<User, String>{
+
+    Optional<User> findByEmail(String email);
+}
